8890db8f667353d80a81c36b221bebb18494ff4c
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-13 12:51:07 +0300

    SQL insert value for Customer table

---
+++ Customer.sql
@@ -0,0 +1,10 @@
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (66, 35, 'Dee dee Peche', 'San Esteban', 'Honduras');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (18, 83, 'Ferd Masdin', 'Enskededalen', 'Sweden');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (71, 58, 'Gabby Scutter', 'Anyar', 'Indonesia');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (95, 10, 'Barny Dibbin', 'Songshan', 'China');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (6, 90, 'Nikki Olivari', 'Shijiao', 'China');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (62, 10, 'Joyan Allmann', 'Norrköping', 'Sweden');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (72, 12, 'Pat Parrott', 'Dikou', 'China');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (59, 55, 'Adel Lougheed', 'Horad Krychaw', 'Belarus');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (49, 12, 'Hamish Mander', 'Oetutulul', 'Indonesia');
+insert into Customer (order_id, customer_id, customer_name, customer_city, customer_country) values (75, 61, 'Danila Kaas', 'Văn Giang', 'Vietnam');
