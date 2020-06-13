e1d42d766f5ddaeab6d1f5f24fd7868f6547086c
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-13 12:51:13 +0300

    SQL insert value for Category table

---
+++ Category.sql
@@ -0,0 +1,10 @@
+insert into Category (category_id, category_name, category_description) values (43, 'Lupinus lyallii A. Gray ssp. alcis-temporis (C.P. Sm.) Cox', 'Toxic effect of venom of gila monster, undetermined, subs');
+insert into Category (category_id, category_name, category_description) values (46, 'Lysimachia waianaeensis H. St. John', 'Failure of steril precaut during aspirat, pnctr and oth cath');
+insert into Category (category_id, category_name, category_description) values (29, 'Lechea deckertii Small', 'Parasitic cyst of retina');
+insert into Category (category_id, category_name, category_description) values (52, 'Desmodium wydlerianum Urb.', 'Laceration w/o fb of r mid finger w damage to nail, subs');
+insert into Category (category_id, category_name, category_description) values (77, 'Curcuma longa L.', 'Blister (nonthermal) of unspecified part of head');
+insert into Category (category_id, category_name, category_description) values (17, 'Ilex opaca Aiton var. arenicola (Ashe) Ashe', 'Unsp complication following incomplete spontaneous abortion');
+insert into Category (category_id, category_name, category_description) values (17, 'Tripterocalyx carneus (Greene) L.A. Galloway', 'Labor and delivery comp by vascular lesion of cord, unsp');
+insert into Category (category_id, category_name, category_description) values (18, 'Puccinellia wrightii (Scribn. & Merr.) Tzvelev', 'Varicos vn unsp low extrm w ulc of ankle and inflammation');
+insert into Category (category_id, category_name, category_description) values (30, 'Crotalaria incana L.', 'Poisoning by hydantoin derivatives, accidental, sequela');
+insert into Category (category_id, category_name, category_description) values (20, 'Musa balbisiana Colla', 'Oth fx head/neck of left femur, init for opn fx type 3A/B/C');
